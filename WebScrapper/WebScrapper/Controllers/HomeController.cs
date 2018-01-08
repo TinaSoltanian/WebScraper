d560e62296f611d458605a8ace5ec0255f340d2c
Tina <tinasoltanian@gmail.com>
2018-01-07 18:41:02 -0800

    return all the gitignore changes it was in correct add soem comments

--- WebScrapper/WebScrapper/Controllers/HomeController.cs
+++ WebScrapper/WebScrapper/Controllers/HomeController.cs
@@ -19,33 +19,48 @@ namespace WebScrapper.Controllers
 {
     public class HomeController : Controller
     {
+        // this is the database context variable
         private DatabaseContext db = new DatabaseContext();
 
         string url = "https://www.dir.ca.gov/dwc/pharmfeesched/pfs.asp";
        
+        // this is get method which is sending request to the url and 
+        // selecting the form from returned html and showing the table inside
         [AcceptVerbs(HttpVerbs.Get)]
         public ActionResult Index() 
         {
+            // sending url to the method and grting back the html document
             HtmlDocument doc = HelperClass.GetPage(url);
 
+            // I wanted to havethe same itle with the original web site
             ViewBag.Title = doc.DocumentNode.CssSelect("h1").First().InnerText;
 
+            // selecting the form with the css class
             HtmlNode node = doc.DocumentNode.CssSelect(".shaded").First();
+
+            // there is a clear form link in the page which is refreshing the same page but 
+            // after rendring the html in my view it has to refresh my view so I replaced the href with 
+            // empty string
             string html = node.InnerHtml.Replace("pfs.asp", "");
+
+            // I set an id for the button so later I can find the button with jQuery on click event
             html = html.Replace("type=\"submit\"", "type =\"submit\" id=\"button\"");
 
+            //store the html text on Viewbag so I can have it in view
             ViewBag.EncodedHtml = MvcHtmlString.Create(html);
 
              return View();
         }       
 
+        // this is the post method which is sending the users's data to original web site
+        // and geting back the result
         [AcceptVerbs(HttpVerbs.Post)]
         public ActionResult CalculatePrice(string NDCno, string MDUnits, 
                                                 string PriceBilled, string DateOfService,
                                                 string NursingHome, string Brand)
         {
 
-           //first I have to register the request
+            // first I have to create request model the request
             Request req = new Request();
             req.NDCNo = NDCno;
             req.MDunits = MDUnits;
@@ -54,6 +69,7 @@ namespace WebScrapper.Controllers
             req.NursingHome = NursingHome.ToUpper() == "TRUE" ? true : false;
             req.Brand = Brand.ToUpper() == "TRUE" ? true : false;              
 
+            // here we need to prepared data to send with the post request
             string data = string.Format("NDCno={0}&MDUnits={1}&PriceBilled={2}&DateOfService={3}",
                 NDCno, MDUnits, PriceBilled, DateOfService);
 
@@ -66,48 +82,56 @@ namespace WebScrapper.Controllers
                 data += "&Brand=on";
             }
 
+            // here i'm setting the request header information
             byte[] dataStream = Encoding.UTF8.GetBytes(data);
             WebRequest webRequest = WebRequest.Create(url);
             webRequest.Method = "POST";
             webRequest.ContentType = "application/x-www-form-urlencoded";
             webRequest.ContentLength = dataStream.Length;  
+
+            // i have to writethe data on request stream to send
             Stream newStream = webRequest.GetRequestStream();
             newStream.Write(dataStream,0,dataStream.Length);
             newStream.Close();
             WebResponse webResponse = webRequest.GetResponse();
 
+            // this method is preparing th html doc from the response from web site
             HtmlDocument responseDoc = HelperClass.PrepareHtmlDocumentFromWebResponse(webResponse);
 
+            // if the user's data is valid so there will be a new table on page with the class of
+            // tabborder
             string html = "";
             Response res = new Models.Response();
             if (responseDoc.DocumentNode.CssSelect(".tabborder").Any())
-            {
+            {                
                 HtmlNode node = responseDoc.DocumentNode.CssSelect(".tabborder").First();
+
+                // this method is reading the <td> nodes os the table and fetching data out of it
+                // and return the response model with the data 
                 res = HelperClass.PrepareResponse(node);
 
+                // here I have to set the response for the request
                 req.Response = res;
                 //res.Request = req;
 
                 html = node.OuterHtml;
             }
 
-            
+            // saving the request and response data in database
             db.Requests.Add(req);
             db.SaveChanges();
 
             return Content(html, "text/html");
         }
 
+        // this is for sending back all data for report
+        // of course this is not the good choice to send all the data but it is just a quick report
+        // for demo
         public ActionResult Report()
         {
             
             return View(db.Requests.ToList());
         }
 
-        public ActionResult ReportDetail(Response model)
-        {
-
-            return PartialView("ReportDetail", model);
-        }
     }
 }
--- WebScrapper/WebScrapper/Helper/HelperClass.cs
+++ WebScrapper/WebScrapper/Helper/HelperClass.cs
@@ -12,6 +12,7 @@ namespace WebScrapper.Helper
 {
     public static class HelperClass
     {
+        // thi is returning the html of the url
         public static HtmlDocument GetPage(string url)
         {
             Uri uri = new Uri(url);
@@ -21,6 +22,7 @@ namespace WebScrapper.Helper
             return PrepareHtmlDocumentFromWebResponse(request.GetResponse());
         }
 
+        // this method is returning the html of the web response
         public static HtmlDocument PrepareHtmlDocumentFromWebResponse(WebResponse response)
         {
             StreamReader reader = new StreamReader(response.GetResponseStream());
@@ -34,6 +36,10 @@ namespace WebScrapper.Helper
             return doc;
         }
 
+        // after reading the values inside <td> there are some extra charachters 
+        // and just one decimal number in each <td>
+        // but I wanted to save the numbers 
+        // so I tried to get just teh numbers out of all thecharachters
         public static string ReturnNUmbersOfString(string text)
         {
             var array = Regex.Split(text, @"[^0-9\.]+").Where(c => c != "." && c.Trim() != "");
@@ -42,22 +48,29 @@ namespace WebScrapper.Helper
             else
                 return string.Empty;
         }
+
         public static Response PrepareResponse(HtmlNode table)
         {
             Response res = new Models.Response();
             List<string> cells = new List<string>();
 
+            // looping through <tr> of teh table
             foreach (var row in table.SelectNodes("tbody/tr"))
             {
+                // each <tr> has couple of <td> which text are there
                 foreach (var cell in row.SelectNodes("td"))
                 {
+                    // tried to remove extra charachaters and $ sign and trim the text
                     cells.Add(cell.InnerText.Replace("\n\r", "").Replace("$","").Trim());
                 }
             }
 
+            // just this two cells had string and decimal value so I need just the decimal value
             cells[8] = ReturnNUmbersOfString(cells[8]);
             cells[14] = ReturnNUmbersOfString(cells[14]);
 
+            // saving the data in the response model 
+            // but I need to convert them to matching data types
             res.LabelName = cells[1];
             res.PriceDate = Convert.ToDateTime(cells[2]);
             res.MDunit = Convert.ToInt32(cells[3]);
